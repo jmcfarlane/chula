86a8e1087ba80017d66b2c436c59f2f3d0d59b1d
John McFarlane <john.mcfarlane@rockfloat.com>
2008-05-27 21:10:45 -0700

    Set default method when root controller without method is used

--- chula/www/mapper/standard.py
+++ chula/www/mapper/standard.py
@@ -15,18 +15,24 @@ class StandardMapper(base.BaseMapper):
 
         # Update package, module, method
         if count == 0:
+            # Homepage
             pass
         elif count == 1:
+            # Root controller using default method
+            self.route.method = base.DEFAULT_METHOD
             self.route.module = parts.pop()
         elif count == 2:
+            # Root controller using specified method
             self.route.method = parts.pop()
             self.route.module = parts.pop()
-            self.route.package +=  '.'.join(parts)
+            self.route.package += '.'.join(parts)
         elif count > 2:
+            # Package controller using specified method
             self.route.method = parts.pop()
             self.route.module = parts.pop()
             self.route.package += '.' + '.'.join(parts)
         else:
+            # This can't happen?
             raise ValueError('Bad route: %s' % self.route)
 
         # The class name is always the capitalized module name
