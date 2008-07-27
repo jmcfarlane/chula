fcac7e8894c2f535ec2dab07653fbff760705543
John McFarlane <john.mcfarlane@rockfloat.com>
2008-07-27 00:34:46 -0700

    Move methods into alpha order; add comments; speed up poll to 1.5

--- chula/queue/server.py
+++ chula/queue/server.py
@@ -15,13 +15,13 @@ from chula.queue.messages import message
 class MessageQueueServer(object):
     def __init__(self, config):
         self.config = config
-        self.poll = 10 
+        self.poll = 1.5
         self.queue = mqueue.MessageQueue(self.config)
         self.pid_file = os.path.join(self.config.mqueue_db, 'server.pid')
         self.log_file = os.path.join(self.config.mqueue_db, 'log')
 
     def consumer(self, msg):
-        print '%s processed by: %s' % (msg.name, thread.get_ident())
+        print ' >>> %s processed by: %s' % (msg.name, thread.get_ident())
         # msg.process()
         self.queue.purge(msg)
         self.log('%s was processed' % msg.name)
@@ -32,17 +32,29 @@ class MessageQueueServer(object):
         log.write('%s: %s\n' % (now, msg))
         log.close()
 
+    def poller(self):
+        while True:
+            msg = self.queue.pop()
+            if msg is None:
+                time.sleep(self.poll)
+            else:
+                thread.start_new_thread(self.consumer, (msg, ))
+
     def producer(self, client):
         chars_left = 1
         msg = ['']
         msg_length = None
 
-        for x in xrange(1000):
+        # Consume the message
+        while True:
             chunk = client.recv(chars_left)
+
+            # End of message
             if chunk == '':
                 client.send('OK ')
                 break
 
+            # Look for the message size
             if msg_length is None:
                 if chunk == ':':
                     try:
@@ -55,6 +67,7 @@ class MessageQueueServer(object):
                 else:
                     msg[0] += chunk
             else:
+                # Once size is known, the rest is the actual message
                 if chars_left > 1:
                     chars_left -= len(chunk)
                 msg.append(chunk)
@@ -78,17 +91,8 @@ class MessageQueueServer(object):
             except:
                 pass
 
-    def poller(self):
-        while True:
-            msg = self.queue.pop()
-            if msg is None:
-                time.sleep(self.poll)
-            else:
-                thread.start_new_thread(self.consumer, (msg, ))
-
     def start(self):
         # Create a pid file
-        daemon = sys.argv[0].rsplit('/', 1)[-1]
         pid = open(self.pid_file, 'w')
         pid.write(str(os.getpid()))
         pid.close()
