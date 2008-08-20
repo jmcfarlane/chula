983c9329da3b617d04da03bb9df96d4ab06ab2ee
John McFarlane <john.mcfarlane@rockfloat.com>
2008-08-19 20:16:28 -0700

    Release of 0.0.4

--- chula/__init__.py
+++ chula/__init__.py
@@ -3,7 +3,7 @@
 Chula is an MVC style framework that works by routing web requests
 thru mod_python to native Python objects"""
 
-__VERSION__ = '0.0.4-alpha03'
+__VERSION__ = '0.0.4'
 version = __VERSION__
 
 package_dir = 'chula'
