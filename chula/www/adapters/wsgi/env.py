f80191ad9213c297792deaeed48c1c05561db1e5
John McFarlane <john.mcfarlane@rockfloat.com>
2008-09-13 18:50:05 -0700

    Fix deepcopy issue specific to wsgi.input and wsgi.errors

--- chula/www/adapters/wsgi/env.py
+++ chula/www/adapters/wsgi/env.py
@@ -27,3 +27,25 @@ class Environment(env.BaseEnv):
 
         # Add additional variables provided by the base class
         super(Environment, self).extras()
+
+    def __deepcopy__(self, memo={}):
+        """
+        Currently not all wsgi.foo objects are easy to deepcopy.  This
+        method overloads BaseEnv to return a fresh object with wsgi
+        input/error objects being "by reference" copies.
+        """
+
+        # Make a copy of the existing objects
+        wsgi_errors = self.wsgi_errors
+        wsgi_input = self.wsgi_input
+
+        # Remove them from the collection
+        self.remove('wsgi_errors')
+        self.remove('wsgi_input')
+
+        # Put them back (by reference)
+        fresh = super(Environment, self).__deepcopy__(memo)
+        fresh.wsgi_input = wsgi_input
+        fresh.wsgi_errors = wsgi_errors
+
+        return fresh
