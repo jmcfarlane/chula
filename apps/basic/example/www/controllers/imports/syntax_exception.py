0aa018e6d4b8a32105b36af19c81faf6843cd532
John McFarlane <john.mcfarlane@rockfloat.com>
2009-10-23 00:59:50 -0700

    Add negative bat test: syntax error in controller

---
+++ apps/basic/example/www/controllers/imports/syntax_exception.py
@@ -0,0 +1,8 @@
+from chula.www import controller
+
+class Syntax_exception(controller.Controller):
+    def index(self):
+        for missing_colon_in_expression in xrange(5)
+            pass
+
+        return 'This will never get called'
